3f29574d6a75d52df39719275810b982aadeb4cf
bhanusisodia4 <34009711+bhanusisodia4@users.noreply.github.com>
2021-08-01 18:01:30 +0530

    Initial commit

---
+++ src/main/java/ArrayListt.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class ArrayListt {
+    public static void main(String[] args) {
+
+        //Type safe
+        ArrayList<String> alist = new ArrayList<>();
+        alist.add("Bhanu");
+        alist.add("Shanu");
+        alist.add("Bhoomi");
+        alist.add("Biitu");
+        alist.add("Shyam");
+        alist.add("Ram");
+
+        //printing through iterable
+
+        for(String s: alist){
+            System.out.print(s+" ");
+        }
+        System.out.println();
+        //removing element
+        alist.remove("Shanu");//removing object
+        alist.remove(3);
+
+        //printing through iterator
+        Iterator itr =alist.iterator();
+        while (itr.hasNext()){
+            System.out.print(itr.next()+" ");
+        }
+        System.out.println();
+        alist.set(0,"mohit"); //setting or changing element at index 0
+
+        //printing through listIterator backward
+
+        ListIterator lst = alist.listIterator(alist.size());
+        while(lst.hasPrevious()){
+            System.out.print(lst.previous()+" ");
+        }
+
+    }
+}
